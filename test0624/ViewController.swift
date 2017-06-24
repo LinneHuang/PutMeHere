c90a8901aa491439b4028f01767922ffd5929328
Linne Huang <linnes.huang@Linnes-iMac.local>
2017-06-24 09:46:23 -0700

    Initial Commit

---
+++ test0624/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test0624
+//
+//  Created by Linne S. Huang on 6/24/17.
+//  Copyright © 2017 Linne S. Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
